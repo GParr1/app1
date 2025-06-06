0b3342bcf6cd4d58242d418e66bbf31f0e27df2f
Giovanni Parrone <gparrone1@pmintl.net>
2025-06-06 11:21:15 +0200

    bootstrap and Logout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 import Login from "./components/Login";
 import Register from "./components/Register";
 
 function App() {
+    const [user, setUser] = useState(null);
+    const auth = getAuth();
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser); // currentUser è null se non loggato
+        });
+
+        return () => unsubscribe(); // pulizia listener
+    }, [auth]);
+    const handleLogout = () => {
+        console.log(user);
+        signOut(auth)
+            .then(() => {
+                console.log("Logout effettuato");
+            })
+            .catch((error) => {
+                console.error("Errore nel logout:", error);
+            });
+    };
   return (
-      <div>
-        <h1>Benvenuto</h1>
-        <Login />
-          <Register/>
+      <div className="container mt-5">
+          {user ? (
+              <div>
+                  <h2>Benvenuto, {user.email}</h2>
+                  <button className="btn btn-danger" onClick={handleLogout}>
+                      Logout
+                  </button>
+              </div>
+          ) : (
+              <>
+                  <h1>Benvenuto</h1>
+                  <Login />
+                  <Register />
+              </>
+          )}
       </div>
   );
 }
