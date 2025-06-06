7530f0c2c38b22c3342af16f04c726c6507e110c
Giovanni Parrone <gparrone1@pmintl.net>
2025-06-06 10:17:08 +0200

    Aggiunto workflow di deploy Firebase

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCacfJ8NeCgqwH_7kI6tMsPLEGRWBaGtkQ",
-    authDomain: "calcetto-fc4b9.firebaseapp.com",
-    projectId: "calcetto-fc4b9",
-    storageBucket: "calcetto-fc4b9.firebasestorage.app",
-    messagingSenderId: "860702319098",
-    appId: "1:860702319098:web:cb0fcdd2cfdf6aa5420443",
-    measurementId: "G-GW7E7XSTL4"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
