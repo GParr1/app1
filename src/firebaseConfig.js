ef135bc994935ffc6d591e63c098b1e8ed51ceb0
Giovanni Parrone <gparrone1@pmintl.net>
2025-06-06 09:40:50 +0200

    firebese config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "TUO_API_KEY",
-    authDomain: "TUO_PROGETTO.firebaseapp.com",
-    projectId: "TUO_PROJECT_ID",
-    storageBucket: "TUO_STORAGE_BUCKET",
-    messagingSenderId: "TUO_MESSAGING_SENDER_ID",
-    appId: "TUO_APP_ID",
+    piKey: "AIzaSyCacfJ8NeCgqwH_7kI6tMsPLEGRWBaGtkQ",
+    authDomain: "calcetto-fc4b9.firebaseapp.com",
+    projectId: "calcetto-fc4b9",
+    storageBucket: "calcetto-fc4b9.firebasestorage.app",
+    messagingSenderId: "860702319098",
+    appId: "1:860702319098:web:cb0fcdd2cfdf6aa5420443",
+    measurementId: "G-GW7E7XSTL4"
 };
 
 const app = initializeApp(firebaseConfig);
