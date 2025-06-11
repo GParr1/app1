bf52113457ab34ff75fc88d10dffe2af3deb7bc7
Giovanni Parrone <gparrone1@pmintl.net>
2025-06-11 15:58:33 +0200

    test collaspe

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -1,42 +1,43 @@
 import React from "react";
 import './css/PayerCard.css';
 
-const PlayerCard = ({playerImage, playerName, playerNumber,
+const PlayerCard = ({
+                        playerImage, playerName, playerNumber,
                         playerColor,
-                        teamSymbol,countryCode,birthDate,height,}) => {
+                        teamSymbol, countryCode, birthDate, height,
+                    }) => {
 
     return (
         <div className="card border-4" style={{
             borderColor: "#f7d500", width: "18rem", position: "relative",
-            background: `repeating-linear-gradient(
-              45deg,
-              ${playerColor.primaryColor},
-              ${playerColor.primaryColor} 20px,
-              ${playerColor.secondaryColor} 20px,
-              ${playerColor.secondaryColor} 50px
-            )`
+            background: `radial-gradient(circle at top left, ${playerColor.primaryColor} 40%, ${playerColor.secondaryColor} 40%)`,
         }}>
             {/* HEADER */}
             <div className="card-header d-flex justify-content-between align-items-center text-white">
                 <span className="fw-bold fs-4"
-                      dangerouslySetInnerHTML={{ __html: teamSymbol }}>
+                      dangerouslySetInnerHTML={{__html: teamSymbol}}>
                 </span>
                 <span className="badge bg-dark fs-6">{countryCode}</span>
                 <span className="fw-bold fs-4">{playerNumber}</span>
             </div>
             {/* IMMAGINE */}
-            {playerImage &&
-                <img src={playerImage} className="card-img-top player-image p-2" alt="Player"/>}
+            <div className='position-absolute'>
+                {playerImage && <img src={playerImage} className="card-img-top player-image p-2" alt="Player"/>}
+                <div className="position-absolute bottom-0 end-0 text-white px-2 py-1 m-2 rounded shadow d-flex align-items-center"
+                     style={{backgroundColor: playerColor.primaryColor}}>
+                    <h5 className="card-title mb-2 mt-2 small text-centred">{playerName}</h5>
+                </div>
+            </div>
 
             {/* INFORMAZIONI */}
-            <div className="card-body text-center">
-                <h5 className="card-title">{playerName}</h5>
-                <p className="card-text small mb-1">{birthDate}</p>
-                <p className="card-text small">{height}</p>
+            <div className="card-body d-flex text-white text-center justify-content-between">
+                <img src="/panini-logo.png" alt="Panini" width="60"/>
+                <p className="card-text small ">{birthDate}</p>
+                <p className="card-text small ">{height}</p>
             </div>
             {/* LOGO PANINI */}
             <div className="position-absolute bottom-0 start-0 m-2">
-                <img src="/panini-logo.png" alt="Panini" width="60"/>
+
             </div>
         </div>
     );
