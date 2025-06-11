a353d4749dfaa26285b447712ae1c16bc5a0da57
Giovanni Parrone <gparrone1@pmintl.net>
2025-06-11 11:38:21 +0200

    fix my accoutn

--- src/App.js
+++ src/App.js
@@ -4,20 +4,15 @@ import { useSelector } from 'react-redux';
 import {ConfirmProfileView} from "./View/ConfirmProfileView";
 import {MyAccountView} from "./View/MyAccountView";
 import {getUser} from "./state/auth/selectors";
-import {doSignOut} from "./utils/authUtils";
 
 function App() {
-
     const user = useSelector(getUser);
     return (
         <div className="container mt-5">
             {user ? (
                 <div>
                     {user.displayName && <MyAccountView user={user}/>}
-                    {user.displayName && <h2 className="text-center mb-4">Benvenuto, {user.displayName || user.email}</h2>}
-                    <button className="btn btn-danger" onClick={doSignOut}>
-                        Logout
-                    </button>
+
                     {/* Se manca displayName → mostra il form per completare il profilo */}
                     {!user.displayName && (
                         <ConfirmProfileView/>
--- src/View/MyAccountView.js
+++ src/View/MyAccountView.js
@@ -3,7 +3,7 @@ import {getUser} from "../state/auth/selectors";
 import {useSelector} from "react-redux";
 import PlayerCard from "../components/PlayerCard";
 import {teamInfo} from "../utils/infoTeam";
-import {authUpdateProfile} from "../utils/authUtils";
+import {authUpdateProfile, doSignOut} from "../utils/authUtils";
 import UploadProfilePicture from "../components/UploadProfilePicture";
 
 // Mock dati di esempio
@@ -21,10 +21,17 @@ export const MyAccountView = () => {
     const user = useSelector(getUser);
     const [selectedTeam, setSelectedTeam] = useState("Juventus");
     const [showModal, setShowModal] = useState(false);
+    const [showModalUpdateImage, setShowModalUpdateImage] = useState(false);
 
     if (!user) return <p>Caricamento profilo...</p>;
-    const openModal = () => setShowModal(true);
-    const closeModal = () => setShowModal(false)
+    const openModal = (type) => {
+        if(type==="updateProfile") setShowModal(true);
+        if(type==="updateImage") setShowModalUpdateImage(true);
+    }
+    const closeModal = () => {
+        setShowModal(false);
+        setShowModalUpdateImage(false);
+    }
 
     const playerColor = teamInfo[selectedTeam].color;
     const teamSymbol = teamInfo[selectedTeam].logo;
@@ -42,11 +49,12 @@ export const MyAccountView = () => {
     }
     return (
         <div className="container mt-5">
-            <h1 className="text-center mb-5">Il Mio Account</h1>
+
+            <h1 className="text-center mb-5">Il Mio Account {user.displayName || user.email}</h1>
 
             <div className="row">
                 {/* Figurina Panini */}
-                <div className="col-md-4 text-center mb-4">
+                <div className="col-md-4 mb-4 d-flex flex-column align-items-center">
                     <PlayerCard
                         playerImage={photoURL}
                         playerName={displayName}
@@ -57,22 +65,44 @@ export const MyAccountView = () => {
                         birthDate="13-5-1993"
                         height="1,91 M"
                     />
-                    <select className="form-select mb-3" value={selectedTeam} onChange={(e) => setSelectedTeam(e.target.value)}>
+                    <select className="mb-3 col-12" value={selectedTeam} onChange={(e) => setSelectedTeam(e.target.value)}>
                         {Object.keys(teamInfo).map((team) => (
                             <option key={team} value={team}>{team}</option>
                         ))}
                     </select>
 
                 </div>
-                <div className="col-md-8">
-                    {/* Bottone per aprire il modale */}
-                    <button className="btn btn-primary mb-3" onClick={openModal}>
-                        Completa il tuo profilo
-                    </button>
-                    {photoURL && <UploadProfilePicture/>}
-                </div>
+
                 {/* Statistiche e Coppe */}
                 <div className="col-md-8">
+                    <div className="d-flex justify-content-between mb-3">
+                        {photoURL && <UploadProfilePicture/>}
+                        {/* Bottone per aprire il modale */}
+                        <div className="d-flex justify-content-end">
+                            <button className="btn btn-primary" onClick={()=>openModal('updateProfile')}>
+                                <svg width="40" height="40" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" role="img" aria-hidden="false">
+                                    <title>Completa il tuo profilo</title>
+                                    <circle cx="12" cy="8" r="4" fill="#4F46E5"></circle>
+                                    <path d="M4 20c0-4 4-6 8-6s8 2 8 6v1H4v-1z" fill="#4F46E5"></path>
+                                </svg>
+                            </button>
+                            <button className="btn btn-danger" onClick={()=>openModal('updateImage')}>
+                                <svg width="40" height="40" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" role="img" aria-hidden="false">
+                                    <title>Cambia immagine del profilo</title>
+                                    <path d="M5 20h14a2 2 0 0 0 2-2v-7l-3-3h-4l-2-2H5a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2z" stroke="#2563EB" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
+                                    <circle cx="12" cy="13" r="3" stroke="#2563EB" strokeWidth="2"></circle>
+                                </svg>
+                            </button>
+                            <button className="btn btn-danger" onClick={doSignOut}>
+                                <svg width="40" height="40" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" role="img" aria-hidden="false">
+                                    <title>Esci dal profilo</title>
+                                    <path d="M16 17L21 12L16 7" stroke="#DC2626" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
+                                    <path d="M21 12H9" stroke="#DC2626" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
+                                    <path d="M12 19C7.58172 19 4 15.4183 4 11C4 6.58172 7.58172 3 12 3" stroke="#DC2626" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
                     <div className="card mb-4 shadow-sm">
                         <div className="card-body">
                             <h5 className="card-title">Statistiche Personali</h5>
@@ -148,6 +178,27 @@ export const MyAccountView = () => {
                                         </button>
                                         <button type="button" className="btn btn-primary" onClick={handleSave}>
                                             Salva
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                    {/* Modale personalizzato */}
+                    {showModalUpdateImage && (
+                        <div className="modal fade show d-block" tabIndex="-1" style={{ backgroundColor: "rgba(0,0,0,0.5)" }}>
+                            <div className="modal-dialog">
+                                <div className="modal-content">
+                                    <div className="modal-header">
+                                        <h5 className="modal-title">Carica la tua immagine</h5>
+                                        <button type="button" className="btn-close" onClick={closeModal}></button>
+                                    </div>
+                                    <div className="modal-body">
+                                        <UploadProfilePicture/>
+                                    </div>
+                                    <div className="modal-footer">
+                                        <button type="button" className="btn btn-secondary" onClick={closeModal}>
+                                            Chiudi
                                         </button>
                                     </div>
                                 </div>
