2c6f3b81caf5c12603f624a90092a704b7fe3750
Giovanni Parrone <gparrone1@pmintl.net>
2025-06-12 10:51:12 +0200

    fix eslint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthView } from './View/AuthView';
 import { useSelector } from 'react-redux';
 import { ConfirmProfileView } from './View/ConfirmProfileView';
 import { MyAccountView } from './View/MyAccountView';
-import { getUser } from './state/auth/selectors';
+import { getUser } from 'state/auth/selectors';
 
 function App() {
   const user = useSelector(getUser);
-  return (
-    <div className="container mt-5">
-      {user ? (
-        <div>
-          {user.displayName && <MyAccountView user={user} />}
 
-          {/* Se manca displayName → mostra il form per completare il profilo */}
-          {!user.displayName && <ConfirmProfileView />}
-        </div>
-      ) : (
-        <AuthView />
-      )}
-    </div>
+  return (
+    <Router>
+      <div className="container mt-5">
+        <Routes>
+          {/* Welcome page */}
+          <Route path="/welcome.html" element={<AuthView />} />
+
+          {/* Profilo completo */}
+          <Route path="/profile.html" element={<MyAccountView user={user} />} />
+
+          {/* Profilo incompleto */}
+          <Route path="/confirm-profile.html" element={<ConfirmProfileView />} />
+
+          {/* Redirect logico in base all'utente */}
+          <Route
+            path="/"
+            element={
+              user ? (
+                user.displayName ? (
+                  <Navigate to="/profile.html" replace />
+                ) : (
+                  <Navigate to="/confirm-profile.html" replace />
+                )
+              ) : (
+                <Navigate to="/welcome.html" replace />
+              )
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
