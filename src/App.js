672ed8cbc3e1addbed8abfae77611c833a3e5d75
Giovanni Parrone <gparrone1@pmintl.net>
2025-06-12 11:34:43 +0200

    fix redirect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthView } from './View/AuthView';
 import { useSelector } from 'react-redux';
 import { ConfirmProfileView } from './View/ConfirmProfileView';
 import { MyAccountView } from './View/MyAccountView';
 import { getUser } from 'state/auth/selectors';
+import { RedirectOnLogin } from 'utils/RedirectOnLogin';
 
 function App() {
   const user = useSelector(getUser);
@@ -13,30 +14,10 @@ function App() {
     <Router basename="/app1">
       <div className="container mt-5">
         <Routes>
-          {/* Welcome page */}
           <Route path="/welcome" element={<AuthView />} />
-
-          {/* Profilo completo */}
           <Route path="/profile" element={<MyAccountView user={user} />} />
-
-          {/* Profilo incompleto */}
           <Route path="/confirm-profile" element={<ConfirmProfileView />} />
-
-          {/* Redirect logico in base all'utente */}
-          <Route
-            path="/"
-            element={
-              user ? (
-                user.displayName ? (
-                  <Navigate to="/profile" replace />
-                ) : (
-                  <Navigate to="/confirm-profile" replace />
-                )
-              ) : (
-                <Navigate to="/welcome" replace />
-              )
-            }
-          />
+          <Route path="/" element={<RedirectOnLogin />} />
         </Routes>
       </div>
     </Router>
---
+++ src/utils/RedirectOnLogin.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { getUser } from 'state/auth/selectors';
+
+export const RedirectOnLogin = () => {
+  const user = useSelector(getUser);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user === undefined) return; // evita redirect prematuro se undefined
+    if (user) {
+      if (user.displayName) {
+        navigate('/profile', { replace: true });
+      } else {
+        navigate('/confirm-profile', { replace: true });
+      }
+    } else {
+      navigate('/welcome', { replace: true });
+    }
+  }, [user, navigate]);
+
+  return <div>Redirecting...</div>; // oppure puoi mostrare uno spinner
+};
