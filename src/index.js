0349c720b1a79f2f285a513e2b6c27326ee0bb09
Giovanni Parrone <gparrone1@pmintl.net>
2025-09-25 16:24:50 +0200

    css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-//import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 import { persistor, store } from 'state/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { logout } from 'state/auth/reducer';
