73bf4dce95333ce6e1e9d61f8f2124e916dedac0
Giovanni Parrone <gparrone1@pmintl.net>
2025-10-17 09:29:28 +0200

    Login Credential View

--- src/View/AuthView.js
+++ src/View/AuthView.js
@@ -1,58 +1,79 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Login from 'components/Login';
 import { labels } from 'properties/authView';
 import { doGoogleLogin } from 'utils/authUtils';
 
 export const AuthView = () => {
-  const [activeTab, setActiveTab] = useState('login');
-
+  const [email, setEamil] = useState(null);
   return (
     <div className="container d-flex flex-column align-items-center justify-content-center mt-5 col-md-6">
-      <img
-        className="header zoom05"
-        aria-hidden="true"
-        src="/app1/assets/logo.png"
-        fetchPriority={'high'}
-        alt={'logo'}
-        decoding="async"
-      />
-      <h1 className="text-center mb-4" id="welcome-heading">
-        {labels.welcome}
-      </h1>
+      <HeaderAuthView title={email ? 'Inserisci la password' : labels.welcome} />
 
-      {/* Pulsanti Social */}
-      <div className="d-flex gap-2 mb-4 flex-wrap justify-content-center">
-        <button
-          className="btn btn-secondary btn-social"
-          onClick={async () => await doGoogleLogin()}
-        >
-          <i className="bi bi-google">
-            <span> Google</span>
-          </i>
-        </button>
-        <button className="btn btn-secondary btn-social">
-          <i className="bi bi-facebook">
-            <span> Facebook</span>
-          </i>
-        </button>
-      </div>
-
-      {/* Divider */}
-      <div className="d-flex align-items-center gap-2 w-100 mb-4">
-        <hr className="flex-grow-1" />
-        <span>o</span>
-        <hr className="flex-grow-1" />
-      </div>
+      {!email && (
+        <>
+          {/* Pulsanti Social */}
+          <SocialLogin />
+          {/* Divider */}
+          <DividerLogin />
+        </>
+      )}
 
       {/* Login / Register Tabs */}
-      <Login />
-      <button
-        className={`nav-link ${activeTab === 'register' ? 'active' : ''}`}
-        type="button"
-        onClick={() => setActiveTab('register')}
-      >
-        {labels.registerTab}
-      </button>
+      <Login setEamil={setEamil} enail={email} />
+      {!email && (
+        <>
+          <button
+            type="button"
+            className="btn btn-secondary w-100"
+            //           onClick={() => navigate('/create-account', { replace: true })}
+          >
+            CREA ACCOUNT
+          </button>
+          <div className="text-center mb-3">
+            <a href={'#password'} className="text-primary-color text-decoration-none">
+              Hai dimenticato la password o devi crearne una nuova?
+            </a>
+          </div>
+        </>
+      )}
     </div>
   );
 };
+
+const HeaderAuthView = title => (
+  <>
+    <img
+      className="header zoom05"
+      aria-hidden="true"
+      src="/app1/assets/logo.png"
+      fetchPriority={'high'}
+      alt={'logo'}
+      decoding="async"
+    />
+    <h1 className="text-center mb-4" id="welcome-heading">
+      {title}
+    </h1>
+  </>
+);
+
+const SocialLogin = () => (
+  <div className="d-flex gap-2 mb-4 flex-wrap justify-content-center">
+    <button className="btn btn-secondary btn-social" onClick={async () => await doGoogleLogin()}>
+      <i className="bi bi-google">
+        <span> Google</span>
+      </i>
+    </button>
+    <button className="btn btn-secondary btn-social">
+      <i className="bi bi-facebook">
+        <span> Facebook</span>
+      </i>
+    </button>
+  </div>
+);
+const DividerLogin = () => (
+  <div className="d-flex align-items-center gap-2 w-100 mb-4">
+    <hr className="flex-grow-1" />
+    <span>o</span>
+    <hr className="flex-grow-1" />
+  </div>
+);
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,11 +2,9 @@ import React, { useState } from 'react';
 import { doGoogleLogin, doSignInWithEmailAndPassword } from 'utils/authUtils';
 import { useNavigate } from 'react-router-dom';
 
-const Login = () => {
+const Login = ({ email, setEmail }) => {
   const navigate = useNavigate();
-  const [email, setEmail] = useState('');
-  const password = '';
-  //const [password, setPassword] = useState('');
+  const [password, setPassword] = useState('');
   const [error, setError] = useState('');
   const [success, setSuccess] = useState('');
 
@@ -34,51 +32,70 @@ const Login = () => {
 
   return (
     <div className="w-100  bg-secondary-bg p-4">
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-          handleLogin('credential');
-        }}
-      >
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label text-primary-text">
-            Telefono o Email
-          </label>
-          <input
-            type="text"
-            id={email}
-            className="form-control bg-primary-bg text-primary-text border-secondary-color"
-            placeholder="Inserisci numero di telefono o e-mail"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div className="form-check mb-3">
-          <input type="checkbox" className="form-check-input" id="rememberMe" />
-          <label className="form-check-label text-primary-text" htmlFor="rememberMe">
-            Ricordami
-          </label>
-        </div>
+      {email && (
+        <>
+          <h4 className="text-center">Inserisci la password di ad*****@blank.com</h4>
+          <div className="mb-3">
+            <label className="otklabel label-uppercase" htmlFor="password">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              aria-describedby="password-form-hint"
+              placeholder="Inserisci la password"
+              autoCorrect="off"
+              autoCapitalize="off"
+              onChange={e => setPassword(e.target.value)}
+              autoComplete="off"
+            />
+          </div>
+          <button
+            type="submit"
+            className="btn btn-primary w-100 mb-3"
+            onClick={e => {
+              handleLogin('credential');
+            }}
+          >
+            ACCEDI
+          </button>
+        </>
+      )}
 
-        <button type="submit" className="btn btn-primary w-100 mb-3">
-          AVANTI
-        </button>
-      </form>
+      {!email && (
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            setEmail(e.target.value);
+          }}
+        >
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label text-primary-text">
+              Telefono o Email
+            </label>
+            <input
+              type="text"
+              id={email}
+              className="form-control bg-primary-bg text-primary-text border-secondary-color"
+              placeholder="Inserisci numero di telefono o e-mail"
+              value={email}
+              //onChange={e => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-check mb-3">
+            <input type="checkbox" className="form-check-input" id="rememberMe" />
+            <label className="form-check-label text-primary-text" htmlFor="rememberMe">
+              Ricordami
+            </label>
+          </div>
 
-      <div className="text-center mb-3">
-        <a href={'#password'} className="text-primary-color text-decoration-none">
-          Hai dimenticato la password o devi crearne una nuova?
-        </a>
-      </div>
-
-      <button
-        type="button"
-        className="btn btn-secondary w-100"
-        onClick={() => navigate('/create-account', { replace: true })}
-      >
-        CREA ACCOUNT
-      </button>
+          <button type="submit" className="btn btn-primary w-100 mb-3">
+            AVANTI
+          </button>
+        </form>
+      )}
 
       {error && <p className="mt-2 text-danger text-center">{error}</p>}
       {success && <p className="mt-2 text-success text-center">{success}</p>}
